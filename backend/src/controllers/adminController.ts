4c4751a2f05f507eb8d5a87346224790bfe82e47
Zolender <ndeingare@gmail.com>
2026-05-03 11:31:30 +0200

    feat: implement deleteUser function to allow admin to delete users

--- backend/src/controllers/adminController.ts
+++ backend/src/controllers/adminController.ts
@@ -19,4 +19,19 @@ export const getAllUsers = async (req: authRequest, res: Response, next: NextFun
 }
 
 
-//deleting a single user
+//deleting a single user(using the id of that one)
+export const deleteUser = async (req: authRequest, res: Response, next: NextFunction)=>{
+    try{
+        const {id} = req.params
+        //admin shouldn't delete themselves
+        if(Number(id)=== req.user!.id){
+            res.status(400).json({message: "You can't delete your own account"})
+            return
+        }
+
+        const result = await pool.query("DELETE FROM users WHERE id=$1 RETURNING *", [id])
+        res.status(200).json({message: `User ${result.rows[0].username} deleted successfully`})
+    }catch(err){
+        next(err)
+    }
+}
