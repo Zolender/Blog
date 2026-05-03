72f399a5a1808d6c3d25f81de6dd6a6c4ae8eb33
Zolender <ndeingare@gmail.com>
2026-05-03 10:23:14 +0200

    feat: add postRoutes to handle CRUD operations and comments/likes for posts

---
+++ backend/src/routes/postRoutes.ts
@@ -0,0 +1,21 @@
+import {Router} from "express"
+import { protect } from "../middleware/authMiddleware.js"
+import { createPost, deletePost, getAllPosts, getPostById, updatePost } from "../controllers/postController.js"
+import { addComment } from "../controllers/commentController.js"
+import { toggleLike } from "../controllers/likeController.js"
+
+
+const router = Router()
+
+//normal post routes
+router.get("/", getAllPosts)
+router.get("/:id", getPostById)
+router.post("/", protect, createPost)
+router.put("/:id", protect, updatePost)
+router.delete("/:id", protect, deletePost)
+
+//comment and likes
+router.post("/:id/comments", protect, addComment)
+router.post("/:id/like", protect, toggleLike)
+
+export default router
