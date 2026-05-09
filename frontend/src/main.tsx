ae44244c25403e0576dbf1c93d1ef53b5eb7c8eb
Zolender <ndeingare@gmail.com>
2026-05-09 10:25:31 +0200

    feat: wrap application in Redux Provider for state management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
+      <></>
+    </Provider>
   </StrictMode>,
 )
