e35d6dd6f587809f11cbc5fbbcadc59fc395d50b
Zolender <ndeingare@gmail.com>
2026-05-09 10:49:49 +0200

    feat: add ProtectedRoute component for user authentication and role-based access control

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,24 @@
+import { Navigate } from "react-router";
+import { useAppSelector } from "../app/hooks";
+
+
+interface Props {
+    children : React.ReactNode
+    adminOnly? : boolean
+}
+
+const ProtectedRoute = ({children, adminOnly = false} : Props) => {
+    const {user, isLoading} = useAppSelector((state)=> state.auth)
+
+    //while rehydratation is happening we don't know yet if the user is logged in or not, no need to start rendering the login page in that case yet
+    if(isLoading)return null
+    //in case the user isn't logged in then, redirection to the login page
+    if(!user) return <Navigate to="/login" replace/>
+    //if the route is only for admins and the user isn't then, redirection to home
+    if(adminOnly && user.role !== "admin")return <Navigate to="/" replace/>
+    
+
+    return <>{children}</>;
+}
+ 
+export default ProtectedRoute;
