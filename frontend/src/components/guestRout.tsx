70883d4b648d85829b8724439cb0deb02d643640
Zolender <ndeingare@gmail.com>
2026-05-10 09:48:49 +0200

    feat: add GuestRoute component for conditional rendering based on user authentication

---
+++ frontend/src/components/guestRout.tsx
@@ -0,0 +1,18 @@
+import type React from "react";
+import { useAppSelector } from "../app/hooks";
+import { Navigate } from "react-router";
+
+
+interface Props {
+    children: React.ReactNode
+}
+
+function GuestRoute({children}: Props){
+    const {user, isLoading} = useAppSelector((state)=> state.auth)
+
+    if(isLoading) return null
+
+    if(user)return <Navigate to="/" replace/>
+
+    return <>{children}</>
+}
