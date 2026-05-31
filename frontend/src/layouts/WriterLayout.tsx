a74ed6e816e0e5efaca1d2b914a1ac6a5f444c0f
Zolender <ndeingare@gmail.com>
2026-05-31 18:44:45 +0200

    feat: add page animation to new and edit post pages through shared layout

--- frontend/src/layouts/WriterLayout.tsx
+++ frontend/src/layouts/WriterLayout.tsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router";
+import {motion} from 'framer-motion'
+
+const pageVariants = {
+    hidden: {opacity: 0, y: 10},
+    visible: {opacity: 1, y: 0, transition: {duration: 0.3}}
+}
 
 const WriterLayout = () => {
     return (
-        <div className="min-h-screen bg-base">
+        <motion.div initial="hidden" animate="visible" variants={pageVariants} className="min-h-dvh bg-base">
             <Outlet/>
-        </div>
+        </motion.div>
     );
 }
  
